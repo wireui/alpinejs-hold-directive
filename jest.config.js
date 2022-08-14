6e9094d4ea06b165d8c5c543e2a36c084fc38f21
PH7-Jack <pedrolivertwd@gmail.com>
2022-08-14 05:03:40 -0300

    setup typescript and jest
    - setup jest extended and types
    - add scripts

---
+++ jest.config.js
@@ -0,0 +1,27 @@
+/*
+ * For a detailed explanation regarding each configuration property and type check, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  clearMocks: true,
+  collectCoverage: false,
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coverageDirectory: 'coverage',
+  coveragePathIgnorePatterns: ['/node_modules/'],
+  coverageProvider: 'v8',
+  moduleNameMapper: {
+    '@/(.*)$': '<rootDir>/src/$1',
+    '@tests/(.*)$': '<rootDir>/tests/$1'
+  },
+  preset: 'ts-jest',
+  roots: [
+    '<rootDir>',
+    'tests',
+    'src'
+  ],
+  setupFilesAfterEnv: ['jest-extended/all'],
+  testEnvironment: 'jsdom',
+  testMatch: ['<rootDir>/tests/**/*.spec.ts']
+}
---
+++ tests/Unit/example.spec.ts
@@ -0,0 +1,5 @@
+describe('Example test', () => {
+  it('should assert true', () => {
+    expect(true).toBeTrue()
+  })
+})
